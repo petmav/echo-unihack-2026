3c8f20f0b2f2bfeb914153d7fe96d7af7d9d7c2d
Briyan <z5463890@ad.unsw.edu.au>
2026-03-14 15:43:58 +1100

    Removed type any from useSpeechRecognition

--- frontend/src/hooks/useSpeechRecognition.ts
+++ frontend/src/hooks/useSpeechRecognition.ts
@@ -20,9 +20,9 @@ interface SpeechRecognition extends EventTarget {
   start(): void;
   stop(): void;
   abort(): void;
-  onresult: ((this: SpeechRecognition, ev: SpeechRecognitionEvent) => any) | null;
-  onerror: ((this: SpeechRecognition, ev: SpeechRecognitionErrorEvent) => any) | null;
-  onend: ((this: SpeechRecognition, ev: Event) => any) | null;
+  onresult: ((this: SpeechRecognition, ev: SpeechRecognitionEvent) => void) | null;
+  onerror: ((this: SpeechRecognition, ev: SpeechRecognitionErrorEvent) => void) | null;
+  onend: ((this: SpeechRecognition, ev: Event) => void) | null;
 }
 
 declare global {
